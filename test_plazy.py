40a6c3f9ea38a58d7bf12cbcbd79d0ca52f155de
L3viathan <git@l3vi.de>
2020-07-14 19:22:08 +0200

    Add simple tests

---
+++ test_plazy.py
@@ -0,0 +1,18 @@
+import plazy
+
+@plazy.me
+def foo(something):
+    return something.escaped
+
+@plazy.me
+def bar():
+    return foo(x / 0)
+
+def test_local_scope():
+    x = 7
+    arg = plazy.Argument("x+3")
+    assert arg.evaluate() == 10
+
+
+def test_escaped():
+    assert bar() == "(x / 0)"
